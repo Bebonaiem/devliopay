8216f89daf331e63d133d4e6f9a17e727cb8b3be
Bebonaiem <bebonaiem@users.noreply.github.com>
2026-07-17 05:25:06 +0300

    fix: implement FilamentUser interface on User model for admin panel access

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
