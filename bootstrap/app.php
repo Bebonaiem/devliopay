4829ca1d158d66fdeb3eda7e30fa2abf69d01894
Bebonaiem <bebonaiem@users.noreply.github.com>
2026-07-19 14:20:41 +0300

    fix: add livewire/* to CSRF exception to resolve 419 on admin edit forms

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->validateCsrfTokens(except: [
             'webhooks/*',
+            'livewire/*',
         ]);
 
         $middleware->alias([
